f08858a047a42a04c3c5cb0b8a1fcf45b1b66f5c
Christian Mann <christian-mann@utulsa.edu>
2012-11-12 22:53:31 -0600

    Moved to X macros, deleted driver

--- data.c
+++ data.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "data.h"
+#define X(a) a, 
 
+enum Symbol {
+	SYMBS
+};
+#undef X
 // data
-NonTerminal firstNonTerminal() {
-	return NT_E;
-}
 int cursor = 0;
 Terminal nextTerminal() {
-	Terminal terms[] = {T_F, T_LPAREN, T_V, T_PLUS, T_V, T_RPAREN, T_PLUS, T_V, T_EOF};
+	Terminal terms[] = {T_PERIOD};
 	int len = sizeof(terms)/sizeof(terms[0]);
 	if(cursor < len) {
 		return terms[cursor++];
 	} else {
 		return terms[len-1];
 	}
-}
-
-bool isTerminal(Symbol s) {
-	return 0 <= s && s < cTerminals;
-}
-
-bool isNonTerminal(Symbol s) {
-	return cTerminals <= s && s < cTerminals + cNonTerminals;
 }
