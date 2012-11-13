cc560b9fe1d88138cd262d915a7bf9bbb4f41d38
Christian Mann <christian-mann@utulsa.edu>
2012-11-12 22:45:47 -0600

    Fixed logic for first/follow sets

--- firstfollow.py
+++ firstfollow.py
@@ -5,6 +5,8 @@ import copy
 
 G = (V, T, S, P)
 H = (Vh, Th, Sh, Ph) = perform_left_factoring(eliminate_left_recursion(G))
+#H = (Vh, Th, Sh, Ph) = (V, T, S, P)
+#print Vh, Th
 
 if __name__ == '__main__':
 	print '\n'.join("%s -> %s" % (l,r) for (l,r) in Ph)
@@ -23,7 +25,7 @@ def firsts():
 					fir[l].update(fir[r[0]])
 				else:
 					i = 0
-					while i < len(r) and '' in fir[r[i]]:
+					while i < len(r) and ('' in fir[r[i]] or r[i] in Th):
 						fir[l].update(fir[r[i]] - set(['']))
 						i += 1
 				if all('' in fir[ri] for ri in r):
@@ -35,20 +37,22 @@ def firsts():
 def follows():
 	fir = firsts()
 	fol = defaultdict(set)
-	fol[Sh].add('$')
+	fol[Sh].add('T_EOF')
 	while True:
 		old = copy.deepcopy(fol)
 		for (l,r) in Ph:
 			for i in range(len(r)):
 				B = r[i]
-				if B in Vh and i < len(r)-1:
+				if B in Vh and i+1 < len(r):
+					if r[i+1] in Th:
+						fol[B].add(r[i+1])
+					else:
+						j = i+1
+						while j < len(r) and '' in fir[r[j]]:
+							fol[B].update(fir[r[j]] - set(['']))
+							j += 1
 
-					j = i+1
-					while j < len(r) and '' in fir[r[j]]:
-						fol[B].update(fir[r[j]] - set(['']))
-						j += 1
-
-					if all('' in ri for ri in r[i:]):
+					if all((ri, '') in Ph for ri in r[i:]):
 						fol[B].update(fol[l])
 				elif B in Vh and i == len(r)-1:
 					fol[B].update(fol[l])
